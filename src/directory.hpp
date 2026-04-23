0ad8bd25e1e9aad42a386e8e3d7940655ce1669a
kaydenmcclung <kaydenmcclung@gmail.com>
2026-04-23 16:04:59 -0500

    set file class as parent
    directory inherits from file class now

--- src/directory.hpp
+++ src/directory.hpp
@@ -3,35 +3,19 @@
 #include <list>
 #include <iostream>
 
-class directory{
+#include "file.hpp"
+
+class directory : public file{
 
 public:
-    //get Functions
-    std::string getName();
-    std::string getOwnerName();
-    int getOwnerUID();
-    std::string getOwnerGroup();
-    int getOwnerGID();
-    int getPerm();
-
-    //set Functions
-    void setOwnerUser(std::string newOwnerName);
-    void setOwnerUser(int newOwnerUID);
-    void setPerm(int newPerm);
-
     void addDirectory(std::string newDirectoryName); 
     void addFile(std::string newFileName);
 
-private:
-    std::string directoryName;
-    std::list<directory> subDirectoryList;
-    //std::list<file> fileList;
-    std::string ownerUser;
-    int ownerUID;
-    std::string ownerGroup;
-    int ownerGID;
-    int perm; //int storing permissions value, hundreth's place = user, tenths = group, and last is other users
+    void delDirectory(std::string directoryName);
+    void delFile(std::string fileName);
 
-    bool checkOctal(int num);
+private:
+    std::list<directory> subDirectoryList;
+    std::list<file> fileList;
 
 };
---
+++ src/file.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <string>
+#include <list>
+#include <iostream>
+
+class file{
+
+public:
+    //get Functions
+    std::string getName();
+    std::string getOwnerName();
+    int getOwnerUID();
+    std::string getOwnerGroup();
+    int getOwnerGID();
+    int getPerm();
+
+    //set Functions
+    void setOwnerUser(std::string newOwnerName);
+    void setOwnerUser(int newOwnerUID);
+    void setPerm(int newPerm);
+
+private:
+    //variables
+    std::string name;
+    std::string ownerUser;
+    int ownerUID;
+    std::string ownerGroup;
+    int ownerGID;
+    int perm; //int storing permissions value, hundreth's place = user, tenth's = group, and one's = other users
+
+    //private functions
+    bool checkOctal(int num);
+
+};
