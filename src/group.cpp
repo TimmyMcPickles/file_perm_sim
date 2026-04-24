b343ed2d9bc735cf5a6685f54898a44e67cd6e53
Ethan P <kingscout1011@gmail.com>
2026-04-24 17:33:10 -0500

    Added functions similar to users to the group class.
    Basic constructor, getter, and setter.
    Creating lists next that will determine user id number and gid based on
    positions in that list

--- src/group.cpp
+++ src/group.cpp
@@ -1,4 +1,13 @@
 #pragma once
 #include "group.hpp"
 
+group::group(std::string name, int id){
+    groupName = name;
+    gid = id;
+}
+
 int group::getGID(){return gid;}
+
+void group::setGID(int id){
+    gid = id;
+}
